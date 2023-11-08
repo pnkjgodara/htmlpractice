7f2040108cb90c6f0593fdc94a5fdc24bf0607b9
pnkjgodara <pankajgodara325@.com>
2023-11-08 13:19:16 +0530

    new code

---
+++ c.programing/avrage.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int a1,b2,c3;
+    float average;
+    printf(" enter the three number =");
+    scanf("%d %d %d",&a1,&b2,&c3);
+
+    average=(a1+b2+c3)/3.0;
+
+
+    
+
+    return 0;
+}
